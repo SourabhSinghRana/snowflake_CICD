408a00d7100307490bd743762041e340abdc4754
Sourabh Singh Rana <128234000+SourabhSinghRana@users.noreply.github.com>
2026-05-27 09:22:23 +0530

    Setup CI/CD environment with roles and database
    This script sets up the environment for CI/CD processes by creating roles, warehouses, databases, and granting necessary permissions. It also includes user creation and schema/table setup for deployment history.

---
+++ SetupEnv/ENV_SETUP.sql
@@ -0,0 +1,85 @@
+USE ROLE ACCOUNTADMIN;
+
+-- 1. Create a custom role for CI/CD process
+CREATE ROLE IF NOT EXISTS CICD_ROLE
+  COMMENT = 'CICD project: Warehouse for CICD workloads';
+
+-- 2. Create a warehouse for the CI/CD process
+CREATE WAREHOUSE IF NOT EXISTS CICD_WH
+  WAREHOUSE_SIZE = 'XSMALL'
+  AUTO_SUSPEND = 60
+  AUTO_RESUME = TRUE
+  COMMENT = 'CICD project: Warehouse for CICD workloads';
+
+-- 3. Create the database
+CREATE DATABASE IF NOT EXISTS CICD_DEMO_DB;
+
+-- 4. Grant privileges to the new role
+GRANT USAGE ON WAREHOUSE CICD_WH TO ROLE CICD_ROLE;
+GRANT USAGE ON DATABASE CICD_DEMO_DB TO ROLE CICD_ROLE;
+GRANT CREATE SCHEMA ON DATABASE CICD_DEMO_DB TO ROLE CICD_ROLE;
+
+
+-- 5. FUTURE GRANTS: Apply permissions automatically to all NEW objects created in the database --
+
+-- Grant comprehensive permissions on all FUTURE schemas
+GRANT ALL PRIVILEGES ON FUTURE SCHEMAS IN DATABASE CICD_DEMO_DB TO ROLE CICD_ROLE;
+
+-- Grant permissions for all types of objects that will be created within FUTURE schemas
+GRANT ALL PRIVILEGES ON FUTURE TABLES IN DATABASE CICD_DEMO_DB TO ROLE CICD_ROLE;
+GRANT ALL PRIVILEGES ON FUTURE VIEWS IN DATABASE CICD_DEMO_DB TO ROLE CICD_ROLE;
+GRANT ALL PRIVILEGES ON FUTURE STAGES IN DATABASE CICD_DEMO_DB TO ROLE CICD_ROLE;
+GRANT ALL PRIVILEGES ON FUTURE FILE FORMATS IN DATABASE CICD_DEMO_DB TO ROLE CICD_ROLE;
+GRANT ALL PRIVILEGES ON FUTURE SEQUENCES IN DATABASE CICD_DEMO_DB TO ROLE CICD_ROLE;
+GRANT ALL PRIVILEGES ON FUTURE STREAMS IN DATABASE CICD_DEMO_DB TO ROLE CICD_ROLE;
+GRANT ALL PRIVILEGES ON FUTURE TASKS IN DATABASE CICD_DEMO_DB TO ROLE CICD_ROLE;
+GRANT ALL PRIVILEGES ON FUTURE PIPES IN DATABASE CICD_DEMO_DB TO ROLE CICD_ROLE;
+GRANT ALL PRIVILEGES ON FUTURE PROCEDURES IN DATABASE CICD_DEMO_DB TO ROLE CICD_ROLE;
+GRANT ALL PRIVILEGES ON FUTURE FUNCTIONS IN DATABASE CICD_DEMO_DB TO ROLE CICD_ROLE;
+
+-- Grants for specific objects like Alerts and Streamlit Apps
+GRANT CREATE ALERT ON ALL SCHEMAS IN DATABASE CICD_DEMO_DB TO ROLE CICD_ROLE;
+GRANT CREATE STREAMLIT ON ALL SCHEMAS IN DATABASE CICD_DEMO_DB TO ROLE CICD_ROLE;
+
+
+-- 6. Create a user
+CREATE USER IF NOT EXISTS CICD_USER
+  DEFAULT_ROLE = CICD_ROLE
+  DEFAULT_WAREHOUSE = CICD_WH
+  MUST_CHANGE_PASSWORD = FALSE;
+
+-- Grant the custom role to the new user
+GRANT ROLE CICD_ROLE TO USER CICD_USER;
+
+
+/* I use Keypair auth so here is have to set it up:
+using OpenSSL on local machine. Run this commands in the terminal
+
+This command generates a private key and encrypts it using the AES 256 algorithm, which will prompt you to enter and verify a passphrase. REMEMBER FOR THIS PASSPHRASE
+openssl genrsa 2048 | openssl pkcs8 -topk8 -inform PEM -out snowflake_cicd_key.p8 -v2 aes-256-cbc
+
+Generates public key:
+openssl rsa -in snowflake_cicd_key.p8 -pubout -out snowflake_cicd_key.pub
+
+So end of the day we have 2 files: 
+.p8 is the private key
+.pub is the publick key
+*/
+
+-- Add the publick key to the user:
+ALTER USER CICD_USER
+    SET RSA_PUBLIC_KEY = 'putyourpublichere_withouttheBEGINandENDPUBLICKEY_and_inoneline';
+
+
+-- 7. Create the TECH schema and the monitoring table for CICD
+CREATE SCHEMA IF NOT EXISTS TECH;
+
+CREATE TABLE IF NOT EXISTS CICD_DEMO_DB.TECH.DEPLOYMENT_HISTORY (
+    DEPLOYMENT_TIMESTAMP TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP(),
+    FILENAME VARCHAR,
+    COMMIT_SHA VARCHAR,
+    GITHUB_ACTOR VARCHAR,
+    STATUS VARCHAR,
+    TYPE VARCHAR, --Type of the deploy. Can be Init(fulldeploy) or normal
+    ERROR_MESSAGE VARCHAR
+);
